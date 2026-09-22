14ec9372de2ba053ff44a6776e9dbf8154f5c739
Lagna. <lagna0175@gmail.com>
2026-09-23 05:47:15 +0700

    feat: publish validated Phase 1 content APIs

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,5 +1,69 @@
 export {
   ContentRegistry,
   type ContentDefinition,
-  type ContentId,
 } from './ContentRegistry';
+
+export {
+  ContentLookupError,
+  canonicalizeContentPackV1,
+  computeContentFingerprintV1,
+  createContentCatalogV1,
+} from './ContentCatalogV1';
+
+export {
+  ContentValidationException,
+  sortContentValidationErrors,
+  type ContentValidationErrorV1,
+  type ContentValidationFailureCodeV1,
+} from './ContentValidationError';
+
+export {
+  CONTENT_FINGERPRINT_ALGORITHM,
+  CONTENT_FORMAT_ID,
+  CONTENT_SCHEMA_VERSION,
+  PHASE1_CONTENT_PACK_ID,
+  PHASE1_CONTENT_PACK_VERSION,
+  type BaseContentDefinitionV1,
+  type ContentCatalogV1,
+  type ContentCompatibilityIdentityV1,
+  type ContentDefinitionV1,
+  type ContentId,
+  type ContentKindV1,
+  type ContentPackV1,
+  type DefinitionForKind,
+  type EntityDefinitionV1,
+  type HazardDefinitionV1,
+  type HostileDefinitionV1,
+  type ItemCapabilityV1,
+  type ItemCategoryV1,
+  type ItemDefinitionV1,
+  type ItemQuantitySpecV1,
+  type ItemUseProfileV1,
+  type LevelThresholdV1,
+  type MachineDefinitionV1,
+  type MilestoneXpRuleV1,
+  type ProfessionDefinitionV1,
+  type ProfessionQuestDefinitionV1,
+  type ProfessionQuestObjectiveV1,
+  type ProgressionDefinitionV1,
+  type ProgressionTriggerV1,
+  type RecipeDefinitionV1,
+  type RepeatXpRuleV1,
+  type ResourceNodeDefinitionV1,
+  type RuinDefinitionV1,
+  type SkillDefinitionV1,
+  type SkillRequirementV1,
+  type StructureDefinitionV1,
+  type StructureRoleV1,
+  type WeatherDefinitionV1,
+} from './SchemaV1';
+
+export {
+  PHASE1_ITEM_IDS,
+  PHASE1_RECIPE_IDS,
+  PHASE1_REQUIRED_CONTENT_IDS,
+  PHASE1_RESOURCE_IDS,
+  PHASE1_STRUCTURE_IDS,
+} from './Phase1Ids';
+
+export { createPhase1ContentCatalog } from './phase1/Phase1Catalog';
