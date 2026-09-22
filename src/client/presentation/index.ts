3b6e387209e53fc98b37ad0ac6f85b17299c9184
Lagna. <lagna0175@gmail.com>
2026-09-22 18:06:39 +0700

    feat: expose presentation public API

---
+++ src/client/presentation/index.ts
@@ -0,0 +1,9 @@
+export {
+  createPixiPresentationAdapter,
+  type PixiPresentationAdapter,
+} from './PixiPresentationAdapter';
+
+export {
+  CameraPresenter,
+  type CameraPresentationPosition,
+} from './CameraPresenter';
