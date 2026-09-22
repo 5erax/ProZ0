277f517cc9995a4df05f52f7f36f54c3e100c2e6
Lagna. <lagna0175@gmail.com>
2026-09-22 20:00:21 +0700

    feat: export movement camera presentation contracts

--- src/client/presentation/index.ts
+++ src/client/presentation/index.ts
@@ -1,9 +1,14 @@
 export {
   createPixiPresentationAdapter,
   type PixiPresentationAdapter,
+  type PixiPresentationOptions,
 } from './PixiPresentationAdapter';
 
 export {
+  CAMERA_FOLLOW_90_TIME_MAX_SECONDS,
+  CAMERA_FOLLOW_90_TIME_SECONDS,
+  CAMERA_MAX_NORMAL_LAG_WU,
   CameraPresenter,
   type CameraPresentationPosition,
+  type CameraPresenterConfig,
 } from './CameraPresenter';
