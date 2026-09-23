f576e8b21736d158e09e8e3a853ed286fb5d89c0
Lagna. <lagna0175@gmail.com>
2026-09-23 17:28:19 +0700

    feat: export Phase 1 death authority

---
+++ src/simulation/death/index.ts
@@ -0,0 +1,2 @@
+export { Phase1DeathAuthority, type DeathTransitionInput, type DeathTransitionResult, type RespawnResult } from './DeathAuthority';
+export type { DeathXpPenaltyPort, DeathXpPenaltyReservation } from './DeathXpPenaltyPort';
