d961732fa3ee7568b68f1ce53959ace64d3a1056
Lagna. <lagna0175@gmail.com>
2026-09-23 17:26:39 +0700

    feat: export Phase 1 survival authority

---
+++ src/simulation/survival/index.ts
@@ -0,0 +1,22 @@
+export {
+  Phase1SurvivalAuthority,
+  type Phase1SurvivalAuthorityOptions,
+} from './SurvivalAuthority';
+
+export { SurvivalGatherCostPort } from './SurvivalGatherCostPort';
+
+export {
+  SURVIVAL_MAX_MILLI,
+  SURVIVAL_STAT_SCALE,
+  type ConsumeRequest,
+  type ConsumeStartResult,
+  type ConsumeTickResult,
+  type DamageEvent,
+  type DamageResult,
+  type PlayerLifeState,
+  type PlayerSurvivalState,
+  type PlayerSurvivalView,
+  type SurvivalAuthoritySnapshot,
+  type SurvivalDamageSource,
+  type SurvivalTickContext,
+} from './SurvivalTypes';
